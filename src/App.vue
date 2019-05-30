984be5e2db68974b9f4f07cf1333ac8ead0fd53c
OyakiMorse <legenda1200@gmail.com>
2019-05-30 14:59:30 +0300

    inite

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <AdminProfile />
+  </div>
+</template>
+
+<script>
+import AdminProfile from './components/AdminProfile';
+
+export default {
+  name: 'App',
+  components: {
+    AdminProfile,
+  },
+};
+</script>
+
+<style lang="stylus">
+</style>
---
+++ src/components/AdminProfile.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="admin-profile">
+    <h1
+      @mouseenter="showList = true"
+      @mouseleave="showList = false"
+    >
+      Hello Mark
+    </h1>
+    <ul v-if="showList">
+      <li>fgfg</li>
+      <li>dfdf</li>
+      <li>fghgh</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminProfile',
+  data() {
+    return {
+      showList: false,
+    };
+  },
+  created() {
+  },
+  methods: {
+    fdfd() {
+      alert(21 - 21);
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+  #admin-profile
+    display flex
+    flex-direction column
+    align-items center
+</style>
